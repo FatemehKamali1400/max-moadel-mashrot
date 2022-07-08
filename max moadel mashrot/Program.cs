e9d2d10e2884f04d304c6ce632b37035c3c6d603
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:26:18 +0430

    tamrin4

---
+++ max moadel mashrot/Program.cs
@@ -0,0 +1,21 @@
+﻿float max=0;
+for (int i = 0; i < 10; i++)
+{
+    System.Console.WriteLine("please Enter avg:");
+   float avg=float.Parse( Console.ReadLine());
+       if (avg<12 && avg>max)
+       {
+         max=avg;
+       }  
+}
+System.Console.WriteLine("max avg mashroot= "+ max) ;
+
+
+
+
+ 
+ 
+
+  
+
+
